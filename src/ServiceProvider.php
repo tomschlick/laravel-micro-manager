07813cba19c78cf30e3033e4718ef901282ecb1f
Tom Schlick <tom@runmybusiness.com>
2016-08-20 17:32:40 -0400

    adding ServiceProvider

---
+++ src/ServiceProvider.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace TomSchlick\MicroManager;
+
+use Illuminate\Support\ServiceProvider as IlluminateServiceProvider;
+
+/**
+ * Class ServiceProvider
+ *
+ * @package TomSchlick\MicroManager
+ */
+class ServiceProvider extends IlluminateServiceProvider
+{
+
+}
---
+++ tests/ServiceProviderTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace TomSchlick\MicroManager\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Class ServiceProviderTest
+ *
+ * @package TomSchlick\MicroManager\Tests
+ */
+class ServiceProviderTest extends TestCase
+{
+
+}
