df227802af1000c5d07d60ce8473c7e4dab932cc
Tom Schlick <tom@runmybusiness.com>
2016-08-20 20:21:41 -0400

    add storage config

--- config/micro-manager.php
+++ config/micro-manager.php
@@ -7,4 +7,10 @@ return [
         'enabled' => true,
         'channel' => 'laravel:queue:jobs',
     ],
+
+    // Define your temporary storage
+    'storage'   => [
+        'driver'       => 'redis',
+        'delete_after' => 180 // minutes
+    ],
 ];
